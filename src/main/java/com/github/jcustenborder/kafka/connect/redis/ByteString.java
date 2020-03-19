9532df3095620c8a4d7d4823ff86e8fb022b4189
Alex Drake <adrake@project44.com>
2020-03-19 11:40:23 -0500

    Removed part of copyright

--- src/main/java/com/github/jcustenborder/kafka/connect/redis/ByteString.java
+++ src/main/java/com/github/jcustenborder/kafka/connect/redis/ByteString.java
@@ -1,5 +1,5 @@
 /**
- * Copyright (c) 2014-2020 project44. All rights reserved.
+ * Copyright (c) 2014-2020 project44
  * <p>
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
